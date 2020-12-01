f3e216db678beeaf169ea047bc9af3ab6493b84f
Paul Kirwan <39994963+paulkirwan6@users.noreply.github.com>
2020-12-01 11:26:16 +0000

    UserInput

--- MazeGame.s
+++ MazeGame.s
@@ -1,49 +1,42 @@
-main:
-    lui x11, 131072     #player start position x-axis
-    addi x12, x0, 52    #player start position y-axis
-    jal x1, createMaze
-	mainLoop:
-        jal x1, moveRight
-        jal x1, moveRight
-        jal x1, moveRight
-        jal x1, moveRight
-        jal x1, moveRight
-        jal x1, moveRight
-        jal x1, moveRight
-		beq x0, x0, mainLoop # loop until reset asserted
+lui x11, 131072     #player start position x-axis
+addi x12, x0, 52    #player start position y-axis
+jal x1, createMaze
+addi x6, x0, 1
+mainLoop:
+    jal x1, userInput
+	beq x0, x0, mainLoop # loop until reset asserted
 
 createMaze: 
-    addi x6, x0, -1         #top/bottom row of maze
-    LUI x7, 524288
-    addi x7, x7, 1          #rows 2-4
-    LUI x8, 1048575
-    addi x8, x8, 497        #row 5 (1)
-    LUI x9, 524288
-    addi x9, x9, 129        #rows 6-8
-    LUI x10, 589823         #row 9 (2)
-    addi x10, x10, 2019        
-    LUI x11, 557056         # rows 10 & 11
-    addi x11, x11, 1
-    LUI x12, 561151         # row 12 (3_)
-    addi x12, x12, 2047
-
+    addi x16, x0, -1         #top/bottom row of maze
+    LUI x17, 524288
+    addi x17, x17, 1          #rows 2-4
+    LUI x18, 1048575
+    addi x18, x18, 497        #row 5 (1)
+    LUI x19, 524288
+    addi x19, x19, 129        #rows 6-8
+    LUI x20, 589823         #row 9 (2)
+    addi x20, x20, 2019        
+    LUI x21, 557056         # rows 10 & 11
+    addi x21, x21, 1
+    LUI x22, 561151         # row 12 (3_)
+    addi x22, x22, 2047
     # store maze design in memory
-    sw x6,  60(x0)
-    sw x7,  56(x0)
-    sw x7,  52(x0)
-    sw x7,  48(x0)
-    sw x8,  44(x0)
-    sw x9,  40(x0)
-    sw x9,  36(x0)
-    sw x9,  32(x0)
-    sw x10,  28(x0)
-    sw x11,  24(x0)
-    sw x11,  20(x0)
-    sw x12,  16(x0)
-    sw x11,  12(x0)
-    sw x11,  8(x0)
-    sw x11,  4(x0)
-    sw x6,  0(x0)
+    sw x16,  60(x0)
+    sw x17,  56(x0)
+    sw x17,  52(x0)
+    sw x17,  48(x0)
+    sw x18,  44(x0)
+    sw x19,  40(x0)
+    sw x19,  36(x0)
+    sw x19,  32(x0)
+    sw x20,  28(x0)
+    sw x21,  24(x0)
+    sw x21,  20(x0)
+    sw x22,  16(x0)
+    sw x21,  12(x0)
+    sw x21,  8(x0)
+    sw x21,  4(x0)
+    sw x16,  0(x0)
     #store maze design in registers
     lw x16, 0(x0)     
     lw x17, 4(x0)  
@@ -62,6 +55,13 @@ createMaze:
     lw x30, 56(x0)    
     lw x31, 60(x0)
     ret              # PC -> x1 (return address)
+
+# Takes 16-bit user input and stores in x4
+userInput:
+    lui x5 0x10
+    lw x4, 0xc(x5)
+
+    beq x4, x6, moveRight
 
 moveRight:
     srli x11, x11, 1
